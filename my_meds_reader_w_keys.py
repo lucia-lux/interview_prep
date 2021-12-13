512590c275f2c3afb0f7245020a26d6c0610c144
lucia-lux <luzia.troebinger.11@ucl.ac.uk>
2021-12-13 18:05:41 +0000

    Added new function which accepts optional input ot extend the types of drugs we can search for.

---
+++ my_meds_reader_w_keys.py
@@ -0,0 +1,55 @@
+# Purpose:  Extract medication/dosage info, allow additional key-value pairs as input.
+# Version:  1
+# Author:   Luzia Troebinger
+# Status:   Active
+# Created:  13-Dec-2021
+
+import re
+
+def my_meds_reader_w_keys(s,**kwargs):
+    """ 
+    Replaces meds names with key (int).
+    Takes additiona key-value pairs as optional args.
+
+    Parameters
+    ----------
+    s:  str
+        input string
+    
+    Returns
+    -------
+    meds_dose_list: list
+        list of lists containing medication code (int) and dose (str)
+        (e.g. [[1,'75mg daily'],[3,'100mg every 2 hours']])
+    """
+    meds_to_search = {'aspirin': 2, 'paracetamol': 1,'panadol': 1,'penicillin': 3}
+ 
+    # if keyword args supplied, add key/value pairs
+    if kwargs:
+        for new_entries in kwargs.values():
+            # names of drugs to search in text
+            meds_to_search.update(new_entries)
+    
+    # lower case everything
+    s = s.lower()
+
+    # split on empty line\line containing only whitespaces.
+    # retain string content prior to first split only (ignore anything below \n\s*\n)
+    s = re.split("\n\s*\n",s)[0]
+
+    # split into individual lines
+    s = s.split('\n')
+
+    # split on first whitespace
+    s_split = [f.split(" ",1) for f in s]
+
+    # replace medication names with key (str)]
+    meds_dose_list=[[meds_to_search[key],f[1]]
+                    if len(f)>1 else 
+                    [meds_to_search[key],""]
+                    for f in s_split
+                    for key in meds_to_search.keys()
+                    if key in f]
+    
+
+    return meds_dose_list
